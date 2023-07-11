b9a2c156cec16ae6afcba1e18c1be983b78de1fe
Phạm Vinh Tiến <133753295+Phamvinhtien@users.noreply.github.com>
2023-07-11 13:26:35 +0700

    ass

---
+++ DEMO/Assignment/Showroom/src/showroom/Menu.java
@@ -0,0 +1,30 @@
+package showroom;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Menu {
+
+    public int int_getChoice(ArrayList<?> options) {
+        Scanner scanner = new Scanner(System.in);
+        int N = options.size();
+
+        for (int i = 0; i < N; i++) {
+            System.out.println((i + 1) + ". " + options.get(i));
+        }
+
+        System.out.print("Please choose an option (1.." + N + "): ");
+        int response = Integer.parseInt(scanner.nextLine());
+        return response;
+    }
+
+    public <E> E ref_getChoice(ArrayList<E> options) {
+        int response;
+        int N = options.size();
+        do {
+            response = int_getChoice(options);
+        } while (response < 1 || response > N);
+        return options.get(response - 1);
+    }
+
+}
