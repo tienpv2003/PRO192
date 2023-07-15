2128d2325ee17e0acea9eade168aaf430360247c
Phạm Vinh Tiến <133753295+Phamvinhtien@users.noreply.github.com>
2023-07-15 22:58:20 +0700

    abc

---
+++ DEMO/Practice PE/Q1/src/Book.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author PhạmVinhTiến
+ */
+public class Book {
+    public String title;
+    public int price;
+
+    public Book() {
+    }
+
+    public Book(String title, int price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    
+}
+
